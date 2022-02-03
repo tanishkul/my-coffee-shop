f5efe8dc20b4f05470019826453187e938ab0733
tanishkul <tanishkul@gmail.com>
2022-02-03 19:46:18 +0530

    Initial changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Component {...pageProps} />
+    <footer>
+      <p>© 2022 Tanish</p>
+    </footer>
+  </>);
 }
 
 export default MyApp
